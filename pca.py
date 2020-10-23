c03a8fa5e8bf5813dfef829b2f0e1d727fcb4078
Zavier-Ong <zacohjayjay@gmail.com>
2020-10-23 14:55:39 +0800

    update gitignore upload test files

---
+++ pca.py
@@ -0,0 +1,12 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from mpl_toolkits.mplot3d import axes3d
+import scipy.spatial.distance as dist
+import plotly
+import plotly.graph_objs as go
+from tqdm import tqdm
+
+print("hello world")
+
