2cc4297901d996cfb15e660532b72bae0c6c8540
Zavier-Ong <zacohjayjay@gmail.com>
2020-10-30 20:15:52 +0800

    complete pca

--- pca.py
+++ pca.py
@@ -12,11 +12,20 @@ folders = ['1', '4', '7', '8', '10', '13', '16', '17', '21', '22',
            '23', '28', '31', '34', '36', '43', '45', '48', '53', '55',
            '57', '61', '64', '67', '68']
 
-train_labels = []
-train_imgs = []
-test_labels = []
-test_imgs = []
+#mixed dataset (PIE + selfie)
+mixed_train_labels = []
+mixed_train_imgs = []
+mixed_test_labels = []
+mixed_test_imgs = []
+#pie only dataset
+pie_train_labels = []
+pie_train_imgs = []
+pie_test_labels = []
+pie_test_imgs = []
+
+#random to ensure reproducibility
 random.seed(6)
+
 for folder in tqdm(folders):
     img_folder = os.path.join('PIE', folder)
     test_index_list = random.sample(range(1, 171), 51)
@@ -26,16 +35,24 @@ for folder in tqdm(folders):
         #30% testing and 70% training (0.3*170 = 51)
         img_name = os.path.splitext(img_file)[0]
         if int(img_name) in test_index_list:
-            test_imgs.append(img)
-            test_labels.append(int(folder))
+            #add to mixed dataset
+            mixed_test_imgs.append(img)
+            mixed_test_labels.append(int(folder))
+            #add to pie dataset
+            pie_test_imgs.append(img)
+            pie_test_labels.append(int(folder))
         else:
-            train_imgs.append(img)
-            train_labels.append(int(folder))
+            #add to mixed dataset
+            mixed_train_imgs.append(img)
+            mixed_train_labels.append(int(folder))
+            #add to pie dataset
+            pie_train_imgs.append(img)
+            pie_train_labels.append(int(folder))
 
-print('Image size: {}'.format(train_imgs[0].shape))
-print('Training set: ' + str(len(train_labels)) + "     Testing set: " + str(len(test_labels)))
+print('Image size: {}'.format(mixed_train_imgs[0].shape))
+print('Training set: ' + str(len(mixed_train_labels)) + "     Testing set: " + str(len(mixed_test_labels)))
 
-#add own selfie photos here
+#selfie only dataset
 own_test_imgs = []
 own_test_labels = []
 own_train_imgs = []
@@ -54,19 +71,20 @@ for img_file in os.listdir('mine'):
         own_train_imgs.append(img)
         own_train_labels.append(26)
 
-train_imgs.extend(own_train_imgs)
-train_labels.extend(own_train_labels)
-test_imgs.extend(own_test_imgs)
-test_labels.extend(own_test_labels)
-print('New Training set: ' + str(len(train_labels)) + "     New Testing set: " + str(len(test_labels)))
+#added selfie to mixed dataset
+mixed_train_imgs.extend(own_train_imgs)
+mixed_train_labels.extend(own_train_labels)
+mixed_test_imgs.extend(own_test_imgs)
+mixed_test_labels.extend(own_test_labels)
+print('New Training set: ' + str(len(mixed_train_labels)) + "     New Testing set: " + str(len(mixed_test_labels)))
 
-train_x = np.array(train_imgs)
-train_y = np.array(train_labels)
-test_x = np.array(test_imgs)
-test_y = np.array(test_labels)
+train_x = np.array(mixed_train_imgs)
+train_y = np.array(mixed_train_labels)
+test_x = np.array(mixed_test_imgs)
+test_y = np.array(mixed_test_labels)
 #vectorize images
-train_x = train_x.reshape(len(train_imgs), -1)
-test_x = test_x.reshape(len(test_imgs), -1)
+train_x = train_x.reshape(len(mixed_train_imgs), -1)
+test_x = test_x.reshape(len(mixed_test_imgs), -1)
 print('Vectorized Training set: {}     Vectorized Test set: {}'.format(train_x.shape, test_x.shape))
 
 #sampling 500
@@ -133,22 +151,43 @@ for i in range(3):
     ax.imshow(eigenfaces[i,:,:])
     ax.set
 
-def doPCA(dimensions):
+
+#vectorize CMU PIE test and selfie sets
+test_pie_x = np.array(pie_test_imgs)
+test_pie_y = np.array(pie_test_labels)
+test_own_x = np.array(own_test_imgs)
+test_own_y = np.array(own_test_labels)
+#vectorize CMU PIE and selfie test images
+test_pie_x = test_pie_x.reshape(len(pie_test_imgs), -1)
+test_own_x = test_own_x.reshape(len(own_test_imgs), -1)
+
+def doPCA(dimensions, isOwn):
     knn = KNN()
     pca = PCA(n_components=dimensions)
-    pca.fit(train_x)
-    train_x_reduced = pca.transform(train_x)
-    knn.fit(train_x_reduced, train_y)
-    test_x_reduced = pca.transform(test_x)
-    result = knn.predict(test_x_reduced)
-    print('Accuracy for PCA {}: {}'.format(dimensions, accuracy_score(test_y, result)))
+    if isOwn:
+        pca.fit(train_x)
+        train_x_reduced = pca.transform(train_x)
+        knn.fit(train_x_reduced, train_y)
+        test_x_reduced = pca.transform(test_own_x)
+        result = knn.predict(test_x_reduced)
+        print('Accuracy for PCA {} on own selfie test: {}'.format(dimensions, accuracy_score(test_own_y, result)))
+    else:
+        pca.fit(train_x)
+        train_x_reduced = pca.transform(train_x)
+        knn.fit(train_x_reduced, train_y)
+        test_x_reduced = pca.transform(test_pie_x)
+        result = knn.predict(test_x_reduced)
+        print('Accuracy for PCA {} on CMU PIE test: {}'.format(dimensions, accuracy_score(test_pie_y, result)))
 
 #PCA 40
-doPCA(40)
+doPCA(40, True)
+doPCA(40, False)
 #PCA 80
-doPCA(80)
+doPCA(80, True)
+doPCA(80, False)
 #PCA 200
-doPCA(200)
+doPCA(200, True)
+doPCA(200, False)
 
 #show 2D, 3D and 3 eigen faces figures
 plt.show()
